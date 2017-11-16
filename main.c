9376cec729d22e2d740f7ba64a6d7de220d99f6b
pandadao <cgsh112@gmail.com>
2017-11-16 05:49:12 -0800

    read all network link and show the matrix of node and link information.

--- main.c
+++ main.c
@@ -13,18 +13,19 @@ int main(int argc, char *argv[]){
 		pFile = fopen("const36", "r");
 
 		int matrix_range = 0;
-		fscan(pFile, "%d\n",&matrix_range);
+		fscanf(pFile, "%d",&matrix_range);
 		
 		
 		/* Initial the matrix to save data about the network state.  */
-		int N = 0, M = 0, MatrixTable[matrix_range][matrix_range]; 
+		int N = 0, M = 0;
+		int MatrixTable[matrix_range][matrix_range];
 		for(N = 0; N < matrix_range; N++){
 		
 				for(M = 0; M < matrix_range; M++){
 				
 						MatrixTable[M][N] = INFINITE;
 				}
-		}
+		}   
 
 		for(N = 0; N < matrix_range; N++){
 		
@@ -33,13 +34,36 @@ int main(int argc, char *argv[]){
 
 	
 		/* Read end node and link number . */
-		int end_node = 0; link_number = 0;
-		fscanf(pFile, "%d\n", &end_node);
-		fscanf(pFile, "%d\n", &link_number);
+		int end_node = 0, link_number = 0;
+		fscanf(pFile, "%d", &end_node);
+		fscanf(pFile, "%d", &link_number);
 
 		
 		/* Add link to MatrixTable.  */
-
+		int rowLine = 0, columnLine = 0;
+		while(fscanf(pFile, "%d %d", &rowLine, &columnLine) != EOF){
 		
+				MatrixTable[rowLine][columnLine] = 1;
+		}
+
+
+		/* close the file and free pointer.  */
+		fclose(pFile);
+		
+		
+		/* Link become 1, others no change their state.    */
+		/* And show the matrix for check symmetry.  */
+		int row = 0, columns = 0;
+		for(row = 0; row<matrix_range;row++){
+		
+				for(columns = 0; columns < matrix_range; columns++){
+				
+						printf("%4d ", MatrixTable[row][columns]);
+				}
+				printf("\n");
+		}
+		
+
+		return 0;
 
 }

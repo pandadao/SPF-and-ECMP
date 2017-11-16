e6a9bce9158513f904ecabb2247839b7ec497518
pandadao <cgsh112@gmail.com>
2017-11-16 04:44:16 -0800

    no such change.

--- main.c
+++ main.c
@@ -15,6 +15,7 @@ int main(int argc, char *argv[]){
 		int matrix_range = 0;
 		fscan(pFile, "%d\n",&matrix_range);
 		
+		
 		/* Initial the matrix to save data about the network state.  */
 		int N = 0, M = 0, MatrixTable[matrix_range][matrix_range]; 
 		for(N = 0; N < matrix_range; N++){
@@ -30,7 +31,15 @@ int main(int argc, char *argv[]){
 				MatrixTable[N][N] = 0;
 		}
 
+	
+		/* Read end node and link number . */
+		int end_node = 0; link_number = 0;
+		fscanf(pFile, "%d\n", &end_node);
+		fscanf(pFile, "%d\n", &link_number);
 
+		
+		/* Add link to MatrixTable.  */
 
+		
 
 }
